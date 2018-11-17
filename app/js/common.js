ca1baae588d1a4069152a233f0bb8c90183e4c18
Serozhka18 <Serozhka1998@gmail.com>
2018-11-17 17:30:59 +0200

    last commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,29 @@
-$(function () {
-   /*$('body').fadeOut();*/ /*Провірка*/
+$('document').ready(function () {
+        let nextCarusell = function () {
+        let currentImage = $('.img.curry');
+        let currentImageIndex = $('.img.curry').index();
+        let nextImageIndex = currentImageIndex + 1;
+        let nextImage = $('.img').eq(nextImageIndex);
+        currentImage.fadeOut(1000);
+        currentImage.removeClass('curry');
+
+        if (nextImageIndex == ($('.img:last').index() + 1)) {
+            $('.img').eq(0).fadeIn(1000);
+            $('.img').eq(0).addClass('curry');
+        } else {
+            nextImage.fadeIn(1000);
+            nextImage.addClass('curry')
+        }
+    };
+
+    let interval = setInterval(nextCarusell, 5000);
+
+    $('.curry button').on('click', stopCarusell);
+
+
+     function stopCarusell()  {
+        clearInterval(interval);
+    }
+
 });
+
